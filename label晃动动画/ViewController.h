32e0c3fb4b8aec15f3639211a98c10efc249136e
ky <ky@kydeiMac.local>
2015-06-12 11:47:47 +0800

    Initial Commit

---
+++ label晃动动画/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  label晃动动画
+//
+//  Created by ky on 15/6/12.
+//  Copyright (c) 2015年 ky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ label晃动动画Tests/label____Tests.m
@@ -0,0 +1,40 @@
+//
+//  label____Tests.m
+//  label晃动动画Tests
+//
+//  Created by ky on 15/6/12.
+//  Copyright (c) 2015年 ky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface label____Tests : XCTestCase
+
+@end
+
+@implementation label____Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
